8f56a87700d14281da556bbb2cf46230aa501aa3
Aki Ariga <ariga@treasure-data.com>
2018-12-15 17:44:03 +0900

    Add td_upload_embulk

--- R/td.R
+++ R/td.R
@@ -40,6 +40,65 @@ td_upload <- function(dbname, table, df, overwrite = FALSE) {
   import_auto(paste(dbname, table, sep = '.'), tmpfname, format = 'tsv', column_types = column_types)
 }
 
+#' Upload data.frame to TD
+#'
+#' @param dbname Target destination database name.
+#' @param table Target table name.
+#' @param df Input data.frame.
+#' @param embulk_dir Path to embulk. [optional]
+#' @param overwrite Flag for overwriting the table if exists. It doesn't overwrite database.
+#'
+#' @examples
+#' \dontrun{
+#' td_upload_embulk("mydb", "iris", iris)
+#'
+#' # With overwrite option
+#' td_upload_embulk("mydb", "iris", iris, overwrite = TRUE)
+#'
+#' # With overwrite option
+#' td_upload_embulk("mydb", "iris", iris, "/path/to/embulk", overwrite = TRUE)
+#' }
+#'
+#' @importFrom readr write_tsv
+#' @export
+td_upload_embulk <- function(dbname, table, df, embulk_dir, overwrite = FALSE) {
+  embulk_exec <- ifelse(missing(embulk_dir), "embulk", file.path(embulk_dir, "embulk"))
+  if(.Platform$OS.type == "windows") {
+    embulk_exec <- paste0(embulk_exec, ".bat")
+  }
+
+  if(Sys.which(embulk_exec) == ''){
+    stop("embulk isn't found. Ensure PATH is set for embulk or use embulk_dir option.")
+  }
+
+  exists_db <- db_exists(dbname)
+  exists_table <- table_exists(dbname, table)
+  if(!overwrite && exists_table) {
+    stop(paste0('"', dbname, ".", table, '" is already exists.'))
+  }
+  if(!exists_db) {
+    db_create(dbname)
+  }
+  if(overwrite && exists_table) {
+    table_delete(dbname, table)
+  }
+  table_create(dbname, table)
+
+  template_path <- system.file("extdata", "tsv_upload.yml.liquid", package="RTD")
+  temp_dir <- tempdir()
+  temp_tsv <- tempfile(fileext = ".tsv", pattern = "embulk_", tmpdir = temp_dir)
+
+  # Replace NA as an empty string
+  readr::write_tsv(df, temp_tsv, na= "")
+  load_yml <- file.path(temp_dir, "load.yml")
+
+  # Set environment variable for embulk
+  Sys.setenv(dbname=dbname, table=table, path_prefix=temp_tsv)
+
+  system2(embulk_exec, paste("guess", template_path, "-o", load_yml))
+  system2(embulk_exec, paste("run", load_yml))
+}
+
 .guess_column_types <- function(df) {
   guessed_types <- sapply(df, function(x){
     if(is.factor(x) || is.character(x)) {
--- tests/testthat/test-td.R
+++ tests/testthat/test-td.R
@@ -20,3 +20,28 @@ test_that("td_upload works with mock",{
   )
   expect_args(m, TRUE, "td", "import:auto --format tsv --auto-create test.iris --column-header --column-types double,double,double,double,string --time-value 1544713200 /tmp/filed7ff398c8e.tsv")
 })
+
+
+
+test_that("td_upload_embulk works with mock",{
+  test_time <- as.POSIXlt("2018-12-13 15:00:00", tz="GMT")
+  template_path <- system.file("extdata", "tsv_upload.yml.liquid", package="RTD")
+  m <- mock(0, 0)
+  with_mock(
+    db_exists = mock(FALSE),
+    table_exists = mock(FALSE),
+    db_create = mock(TRUE),
+    table_create = mock(TRUE),
+    table_delete = mock(TRUE),
+    tempdir = mock("/tmp"),
+    `system.file` = mock("/home/R/package/RTD/extdata/tsv_upload.yml.liquid"),
+    `Sys.which` = mock("/home/RTD/bin/embulk"),
+    `Sys.time` = mock(test_time),
+    `system2` = m,
+    {
+      td_upload_embulk("test", "iris", iris)
+    }
+  )
+  expect_args(m, 1, "embulk", paste("guess", template_path, "-o /tmp/load.yml"))
+  expect_args(m, 2, "embulk", "run /tmp/load.yml")
+})
